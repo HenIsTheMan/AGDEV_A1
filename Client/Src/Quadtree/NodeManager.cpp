95f698930f3968e3309a83521345a03d8139a68c
Henry <193541T@mymail.nyp.edu.sg>
2020-12-19 11:56:04 +0800

    Updated NodeManager

--- Client/Src/Quadtree/NodeManager.cpp
+++ Client/Src/Quadtree/NodeManager.cpp
@@ -1,44 +1,17 @@
 #include "NodeManager.h"
 
-std::vector<Node*> NodeManager::activeNodes;
-std::vector<Node*> NodeManager::inactiveNodes;
-
 NodeManager::~NodeManager(){
-	for(Node*& node: activeNodes){
-		if(node != nullptr){
-			delete node;
-			node = nullptr;
-		}
-	}
-
-	for(Node*& node: inactiveNodes){
-		if(node != nullptr){
-			delete node;
-			node = nullptr;
-		}
+	if(nodePool != nullptr){
+		delete nodePool;
+		nodePool = nullptr;
 	}
 }
 
 void NodeManager::Init(const size_t& inactiveSize, const size_t& activeSize){
-	activeNodes.reserve(activeSize);
-
-	inactiveNodes.reserve(inactiveSize);
-	for(size_t i = 0; i < inactiveSize; ++i){
-		inactiveNodes.emplace_back(new Node());
-	}
+	nodePool->Init(inactiveSize, activeSize);
 }
 
-Node* NodeManager::ActivateNode(){
-	Node* const node = inactiveNodes.front();
-	activeNodes.emplace_back(node);
-	inactiveNodes.erase(inactiveNodes.begin());
-	return node;
-}
-
-void NodeManager::DeactivateNode(const Node* const node){
-	std::vector<Node*>::iterator iter = std::find(activeNodes.begin(), activeNodes.end(), node);
-	if(iter != activeNodes.end()){
-		inactiveNodes.emplace_back(*iter);
-		activeNodes.erase(iter);
-	}
+NodeManager::NodeManager():
+	nodePool(ObjPool<Node>::GetObjPtr())
+{
 }
--- Client/Src/Quadtree/NodeManager.h
+++ Client/Src/Quadtree/NodeManager.h
@@ -4,18 +4,16 @@
 
 #include "Node.h"
 
+#include "../ObjPool/ObjPool.h"
+
 class NodeManager final: public Singleton<NodeManager>{
 	friend Singleton<NodeManager>;
 public:
 	~NodeManager();
 
 	void Init(const size_t& inactiveSize, const size_t& activeSize);
-
-	Node* ActivateNode();
-	void DeactivateNode(const Node* const node);
 private:
-	static std::vector<Node*> activeNodes;
-	static std::vector<Node*> inactiveNodes;
+	ObjPool<Node>* nodePool;
 
-	NodeManager() = default;
+	NodeManager();
 };
