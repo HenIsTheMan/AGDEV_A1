e298b28695e5d31efd4f6d0a9193d90183cac177
Henry <193541T@mymail.nyp.edu.sg>
2020-12-19 11:29:43 +0800

    NodeManager

--- Client/Src/Quadtree/NodeManager.cpp
+++ Client/Src/Quadtree/NodeManager.cpp
@@ -1,2 +1,44 @@
 #include "NodeManager.h"
 
+std::vector<Node*> NodeManager::activeNodes;
+std::vector<Node*> NodeManager::inactiveNodes;
+
+NodeManager::~NodeManager(){
+	for(Node*& node: activeNodes){
+		if(node != nullptr){
+			delete node;
+			node = nullptr;
+		}
+	}
+
+	for(Node*& node: inactiveNodes){
+		if(node != nullptr){
+			delete node;
+			node = nullptr;
+		}
+	}
+}
+
+void NodeManager::Init(const size_t& inactiveSize, const size_t& activeSize){
+	activeNodes.reserve(activeSize);
+
+	inactiveNodes.reserve(inactiveSize);
+	for(size_t i = 0; i < inactiveSize; ++i){
+		inactiveNodes.emplace_back(new Node());
+	}
+}
+
+Node* NodeManager::ActivateNode(){
+	Node* const node = inactiveNodes.front();
+	activeNodes.emplace_back(node);
+	inactiveNodes.erase(inactiveNodes.begin());
+	return node;
+}
+
+void NodeManager::DeactivateNode(const Node* const node){
+	auto iter = std::find(activeNodes.begin(), activeNodes.end(), node);
+	if(iter != activeNodes.end()){
+		inactiveNodes.emplace_back(*iter);
+		activeNodes.erase(iter);
+	}
+}
--- Client/Src/Quadtree/NodeManager.h
+++ Client/Src/Quadtree/NodeManager.h
@@ -2,9 +2,20 @@
 
 #include <Engine.h>
 
+#include "Node.h"
+
 class NodeManager final: public Singleton<NodeManager>{
 	friend Singleton<NodeManager>;
 public:
+	~NodeManager();
+
+	void Init(const size_t& inactiveSize, const size_t& activeSize);
+
+	Node* ActivateNode();
+	void DeactivateNode(const Node* const node);
 private:
-	//static std::vector
+	static std::vector<Node*> activeNodes;
+	static std::vector<Node*> inactiveNodes;
+
+	NodeManager() = default;
 };
